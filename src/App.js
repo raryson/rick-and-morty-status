7538262037a62a718da31b32dfa801bef3de8abe
Raryson Pereira Rost <raryson.rost@gmail.com>
2019-05-03 15:25:39 -0300

    Create multi render characters and colorful status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import Col from 'react-bootstrap/Col'
 import Character from './components/Character'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import axios from 'axios'
 import './App.css'
 
@@ -18,13 +20,17 @@ class App extends React.Component {
   }
 
  render() {
-  
 
   return (
     <div className="App">
-      {this.state.characters.map((character, index) => {
-        return <Character name={character.name} key={index} status={character.status}/>
-      })}
+      <h1 className="title">Rick and Morty Status Catalog</h1>
+      <Container>
+        <Row>
+          {this.state.characters.map((character, index) => {
+                return <Character name={character.name} key={index} status={character.status} image={character.image}/>
+          })}
+        </Row>
+      </Container>
     </div>
   );
  }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
-const Character = ({name, status}) => {
+
+const Character = ({name, status, image}) => {
     return (
-        <div>
-             <p>Character {name} is {status}</p>
-        </div>
+        <Col>
+            <Card style={{ margin: '10px', width: '18rem'}}>
+                <Card.Img variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text className={status === 'Alive' ? 'alive' : 'dead'}>
+                        {status}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
     )
 }
 
